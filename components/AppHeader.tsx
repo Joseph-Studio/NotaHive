bbc516bb9e00961d9b1c58405f075f3fb8c9afd5
Rezakori <66035933+RonaldoK618@users.noreply.github.com>
2025-07-29 11:43:54 -0600

    Changes to AppHeader and new logo

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -4,8 +4,8 @@ import { View, Text, Image, StyleSheet } from 'react-native';
 export default function AppHeader() {
   return (
     <View style={styles.header}>
-      <Text style={styles.title}>Welcome{'\n'}To{'\n'}Students{'\n'}Notes</Text>
-      <Image source={require('../assets/shrek.png')} style={styles.image} />
+      <Text style={styles.title}>Welcome{'\n'}To</Text>
+      <Image source={require('../assets/NotaHiveLogo2.png')} style={styles.image} />
     </View>
   );
 }
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   image: {
-    width: 120,
-    height: 120,
+    width: 300,
+    height: 300,
   },
 });
